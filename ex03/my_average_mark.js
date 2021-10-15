33b83e99fba1b429d973ff32287af1223e95319c
jacqueline ocana <ocana_j@qwasar.io>
2021-10-15 06:35:06 +0000

    ex03 hash tables

---
+++ ex03/my_average_mark.js
@@ -0,0 +1,39 @@
+
+function my_average_mark() {
+    var hash = (string, max) => {
+        var hash = 0;
+        for (var i = 0; i < string.length; i++){
+            hash += string.charCodeAt(i);
+        }
+        //index will place item into array
+        return hash % max;
+    };
+    
+    let HashTable = function(){
+        let storage = [];
+        const storageLimit = 4;
+    
+        this.print = function(){
+            console.log(storage);
+        }
+    }
+    this.add = function(key, value){
+        var index = hash(key,storageLimit);
+        if(storage[index] === undefined){
+            storage[index] = [
+                [key,value]
+            ];
+        }else{
+            var inserted = false;
+            for(var i = 0; i <storage[index].length; i++){
+                if(storage[index[i[0]] === key]){
+                    storage[index[i[1]]  = value;
+                    inserted = true;
+                }
+            }
+        }
+        if (inserted === false){
+            storage[index].push([key,value]);
+        }};
+};
+
