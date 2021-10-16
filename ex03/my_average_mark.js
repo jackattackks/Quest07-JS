96fb2a674fac94dcad189f95f9309e70ca57d61a
jacqueline ocana <ocana_j@qwasar.io>
2021-10-16 18:01:35 +0000

    finished hash tables excercise

--- ex03/my_average_mark.js
+++ ex03/my_average_mark.js
@@ -7,3 +7,18 @@
 // const person = [{}
 // person ['name'] = 'bob';
 // person ['grade'] = 65;
+
+function my_average_mark(array) {
+// taking in an array 
+// total is initialized to 0, will use later to get average
+    var total = 0;
+// below iterate through the array with for loop
+    for (i = 0; i < array.length; i++){
+// going through the array and look for the KEY (integer) 
+// grab integer key and add it to total
+        total += array[i]['integer'];
+    }
+// taking the sum of all the grades (total) and dividing by the length
+// need to set precision to only one decimal    
+    return (total / array.length).toFixed(1);
+}
